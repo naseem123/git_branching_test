0fbe58a835249134efef1984e49fb93823bda5c9
naseem-dev <naseem@prismcloudinc.com>
2024-10-31 00:04:45 +0530

    signup fix

---
+++ lib/features/signup/signup_second.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SignupSecondScreen extends StatefulWidget {
+  const SignupSecondScreen({super.key});
+
+  @override
+  State<SignupSecondScreen> createState() => _SignupSecondScreenState();
+}
+
+class _SignupSecondScreenState extends State<SignupSecondScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
